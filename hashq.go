6a6ed1aa2efe67a05639da4823a941aae0ccc6e6
Alexander Zaytsev <thebestzorro@yandex.ru>
2015-04-19 13:35:43 +0300

    addded tests

--- hashq.go
+++ hashq.go
@@ -16,11 +16,19 @@ import (
 )
 
 var (
-    // MaxShared is a number of shared resources.
-    MaxShared int64 = 4
-    // SpeedCheck is parameter to check time interval of incoming requests.
-    SpeedCheck uint64 = 10
+    // SharedMap is an array of shared resources wrappers.
+    SharedMap []*Resource
 
+    // emptyPt is an example of default Shared object
+    emptyPt Shared
+    // cleanPeriod is an interval between attempts to clean resources.
+    cleanPeriod = time.Second * 60
+    // olderPeriod is an interval after wich a resource can be considered obsolete.
+    olderPeriod = time.Second * 30
+    // maxShared is a number of shared resources.
+    maxShared int64 = 4
+    // speedCheck is parameter to check time interval between incoming requests.
+    speedCheck uint64 = 10
     // curSpeed is a speed structure of incoming requests.
     curSpeed *Speed
     // incomeFreq is initialize value of incoming requests.
@@ -34,14 +42,15 @@ var (
 
 // Shared is an interface of a shared resource.
 type Shared interface {
-    Open() (*interface{}, error)
+    Open() (Shared, error)
     Close()
 }
 
 // Resource is a wrapper of shared resource to
 // safe handle its operations.
 type Resource struct {
-    Res      *Shared
+    Pt       Shared
+    active   bool
     open     sync.Once
     mutex    sync.RWMutex
     modified time.Time
@@ -64,23 +73,92 @@ func Debug(debug bool) {
     loggerDebug = log.New(debugHandle, "DEBUG [hashq]: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
 }
 
+// Init initializes shared map:
+//
+// size - a number of shared resources;
+// spch - a parameter to check time interval between incoming requests;
+// cleaner - an interval between attempts to clean resources;
+// older - a resource can be considered obsolete after this interval.
+func Init(share Shared, size int64, spch uint64, cleaner, older time.Duration) {
+    emptyPt = share
+    maxShared, speedCheck, cleanPeriod, olderPeriod = size, spch, cleaner, older
+    SharedMap = make([]*Resource, maxShared)
+    for i := range SharedMap {
+        SharedMap[i] = &Resource{}
+        SharedMap[i].Pt = emptyPt
+    }
+    go func() {
+        ticker := time.Tick(cleanPeriod)
+        for {
+            select {
+                case <-ticker:
+                    Clean()
+            }
+        }
+    }()
+    InitSpeed()
+}
+
 // InitSpeed initializes new internal Speed structure.
 // This method should be called before the first requests.
 func InitSpeed() {
     initTime := time.Now().UnixNano()
     curSpeed = &Speed{0, initTime, initTime}
 }
+
+// Clean resets unused resources. Only this method can delete shared pointers.
+func Clean() {
+    loggerDebug.Println("start Clean()")
+    defer loggerDebug.Println("end Clean()")
+
+    for i, res := range SharedMap {
+        if (res.active) && (time.Now().Sub(res.modified) > olderPeriod) {
+            loggerDebug.Printf("try to close %v\n", i)
+            res.Clean(i)
+        }
+    }
+}
+
+// genHash returns a hash index for new incoming request.
+func genHash() int64 {
+    curSpeed.inc()
+    if curSpeed.Check() {
+        if f := curSpeed.Freq(); f != 0 {
+            incomeFreq = f
+            loggerDebug.Printf("incomeFreq was corrected: %v\n", f)
+        }
+    }
+    idx := (time.Now().UnixNano() / incomeFreq) % maxShared
+    loggerDebug.Printf("get idx=%v\n", idx)
+    return idx
+}
+
+// Get returns a shared resource.
+func Get() *Resource {
+    idx := genHash()
+    res := SharedMap[idx]
+    loggerDebug.Printf("Get() returned [%v]=%v", idx, &SharedMap[idx])
+    return res
+}
+
+func Stat() {
+    loggerDebug.Println("Stat")
+    for i, res := range SharedMap {
+        loggerDebug.Printf("\t %v: pt=%v\n", i, res.Pt)
+    }
+}
+
 // inc increments counters of Speed structure.
 func (s *Speed) inc() {
     s.Sum, s.Last = s.Sum + 1, time.Now().UnixNano()
 }
 // Avg returns an average speed of incoming requests.
-func (s *Speed) Avg() float64 {
-    if s.Last == s.Start {
-        return 0
-    }
-    return s.Sum / float64(s.Last - s.Start)
-}
+// func (s *Speed) Avg() float64 {
+//     if s.Last == s.Start {
+//         return 0
+//     }
+//     return s.Sum / float64(s.Last - s.Start)
+// }
 // Freq returns an average frequency of incoming requests
 func (s *Speed) Freq() int64 {
     if s.Sum == 0 {
@@ -90,5 +168,47 @@ func (s *Speed) Freq() int64 {
 }
 // Check verifies that frequency should be corrected.
 func (s *Speed) Check() bool {
-    return (uint64(s.Sum) % SpeedCheck) == 1
+    return (uint64(s.Sum) % speedCheck) == 1
+}
+
+// Clean reset an unused resource and "close" shared object.
+func (res *Resource) Clean(i int) {
+    res.mutex.Lock()
+    defer res.mutex.Unlock()
+
+    res.Pt.Close()
+    res.Pt, res.open, res.active = emptyPt, sync.Once{}, false
+    res.touch()
+    loggerDebug.Printf("%v is closed\n", i)
+    Stat()
+}
+
+// Lock implements read-lock for a resource.
+// If Lock was called then a resource can't be closed.
+func (res *Resource) Lock() {
+    res.touch()
+    res.mutex.RLock()
+}
+
+// Lock implements read-unlock for a resource.
+func (res *Resource) Unlock() {
+    res.touch()
+    res.mutex.RUnlock()
+}
+
+// touch updates last modified time of a resource object.
+func (res *Resource) touch() {
+    res.modified = time.Now()
+}
+
+// TryOpen calls Open() method of shared resource only once.
+func (res *Resource) TryOpen() (Shared, error) {
+    var err error = nil
+    open := func() {
+        loggerDebug.Println("called TryOpen()")
+        res.Pt, err = res.Pt.Open()
+        res.active = true
+    }
+    res.open.Do(open)
+    return res.Pt, err
 }
--- hashq_test.go
+++ hashq_test.go
@@ -5,8 +5,96 @@
 package hashq
 
 import (
+    "fmt"
+    "math/rand"
     "testing"
+    "time"
 )
+
+var (
+    sharedNum int64 = 4
+    checkFreq uint64 = 5
+    cleanerTime = time.Millisecond * 30
+    olderTime = time.Millisecond * 20
+
+    minOpenTime = time.Millisecond * 10
+    maxOpenTime int64 = 5
+
+    maxOpenValue int64 = 100
+    waitError bool
+
+    minReqTime = time.Millisecond * 5
+    maxReqTime int64 = 30
+
+    maxRequests int = 50
+    minRequestDelay = time.Millisecond * 5
+    maxRequestDelay int64 = 5
+)
+
+type Connection struct {
+    Id     int64
+    Active bool
+}
+type Request struct {
+    Id      int64
+    Con     *Connection
+    Created time.Time
+}
+type Result struct {
+    Answer int64
+    Err    error
+}
+
+func (con *Connection) Open() (Shared, error) {
+    rand.Seed(time.Now().UnixNano())
+    salt := time.Duration(rand.Int63n(maxOpenTime)) * time.Millisecond
+    time.Sleep(minOpenTime + salt)
+
+    c := &Connection{rand.Int63n(maxOpenValue), true}
+    if waitError {
+        return c, fmt.Errorf("expected error")
+    }
+    return c, nil
+}
+func (con *Connection) Close() {
+    con.Id = 0
+    con.Active = false
+}
+func (req *Request) Run(t *testing.T, result chan Result) {
+    rand.Seed(time.Now().UnixNano())
+    // it is some shared resource
+    res := Get()
+    res.Lock()
+    loggerDebug.Printf("run start for %v\n", &res)
+    defer func() {
+        loggerDebug.Printf("run end for %v\n", &res)
+        res.Unlock()
+    }()
+
+    sharedCon, err := res.TryOpen()
+    if err != nil {
+        result<-Result{0, err}
+        return
+    }
+    con := sharedCon.(*Connection)
+    if !con.Active {
+        t.Errorf("inactive connection was used")
+    }
+    salt := time.Duration(rand.Int63n(maxReqTime)) * time.Millisecond
+    time.Sleep(minReqTime + salt)
+    result<-Result{req.Id * con.Id, nil}
+}
+func ReqGenerator(req chan Request) {
+    rand.Seed(time.Now().UnixNano())
+    reqNum := rand.Int63n(256)
+    for i := 0; i < maxRequests; i++ {
+        req <- Request{reqNum, &Connection{}, time.Now()}
+        reqNum++
+        salt := time.Duration(rand.Int63n(maxRequestDelay)) * time.Millisecond
+        time.Sleep(minRequestDelay + salt)
+    }
+    close(req)
+}
 
 func TestDebug(t *testing.T) {
     if (loggerError == nil) || (loggerDebug == nil) {
@@ -20,4 +108,41 @@ func TestDebug(t *testing.T) {
     if (loggerError.Flags() != 19) || (loggerDebug.Flags() != 21) {
         t.Errorf("incorrect loggers settings")
     }
+}
+
+func TestInit(t *testing.T) {
+    Debug(true)
+    empty := &Connection{}
+    Init(empty, sharedNum, checkFreq, cleanerTime, olderTime)
+
+    finish := make(chan bool)
+    resultCh := make(chan Result)
+    reqCh := make(chan Request)
+
+    go func() {
+        i := 0
+        for result := range resultCh {
+            if result.Err != nil {
+                t.Errorf("result error: %v", result.Err)
+            }
+            if result.Answer == 0 {
+                t.Errorf("incorrect answer")
+            }
+            i++
+            if i >= maxRequests {
+                finish <- true
+                return
+            }
+        }
+    }()
+
+    go ReqGenerator(reqCh)
+    for req := range reqCh {
+        reqpt := &req
+        go reqpt.Run(t, resultCh)
+    }
+
+    done := <-finish
+    close(resultCh)
+    t.Log("all done", done)
 }
